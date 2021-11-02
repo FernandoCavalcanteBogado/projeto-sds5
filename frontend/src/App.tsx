697039a6411bc00403dfd9dc2859f7999defbf60
Fernando Bogado <bogadinho@gmail.com>
2021-11-02 16:06:41 -0400

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import NavBar from "components/NavBar";
+import BarChart from "components/NavBar/BarChart";
 import DataTable from "components/NavBar/DataTable";
+import DonutChart from "components/NavBar/DonutChart";
 import Footer from "components/NavBar/Footer";
 
 function App() {
@@ -7,7 +9,24 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Powered by Fernando Bogado</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Todas as vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+            <h2 className="text-primary">Todas vendas</h2>
+
+        
+        </div>
         <DataTable />
       </div>
       <Footer />
